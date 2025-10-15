8cff213f25925f3d8d59f19a3452fdcdc7584c44
Yury Bushmelev <jay4mail@gmail.com>
2025-10-15 12:13:27 +0800

    chore: simplify build scripts (+drop telemetry)

--- build.sh
+++ build.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
-
-export WORKINGDIR=$(pwd)
-
 target=${1:-build}
 arch=$(go env GOHOSTARCH)
 platform=$(go env GOHOSTOS)
 binPath="$(pwd)/dist/pct_${platform}_${arch}"
-binPath2="$(pwd)/dist/notel_pct_${platform}_${arch}"
 
 amd64=$(go env GOAMD64)
-if [[ ! -z "${amd64+z}" ]]; then
+if [[ -n "$amd64" ]]; then
 	binPath="${binPath}_${amd64}"
-	binPath2="${binPath2}_${amd64}"
 fi
 
 if [ "$target" == "build" ]; then
   # Set goreleaser to build for current platform only
-  if [ -z "${HONEYCOMB_API_KEY}" ]; then
-    export HONEYCOMB_API_KEY="not_set"
-  fi
-  if [ -z "${HONEYCOMB_DATASET}" ]; then
-    export HONEYCOMB_DATASET="not_set"
-  fi
   goreleaser build --snapshot --clean --single-target
   git clone -b main --depth 1 --single-branch https://github.com/puppetlabs/baker-round "$binPath/templates"
-  cp -r "$binPath/templates" "$binPath2/templates"
 elif [ "$target" == "quick" ]; then
-  go build -o ${binPath}/pct -tags telemetry
-  go build -o ${binPath2}/pct
+  go build -o "${binPath}"/pct
 elif [ "$target" == "package" ]; then
   git clone -b main --depth 1 --single-branch https://github.com/puppetlabs/baker-round "templates"
-  goreleaser --skip-publish --snapshot --rm-dist
+  goreleaser --skip-publish --snapshot --clean
 fi
